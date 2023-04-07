31d5bfaf32fae7577c807087951785813656d7c4
Alex Moriyama <alex.moriyama@gmail.com>
2023-04-06 22:11:19 -0700

    Had to redo my files because I cloned the starter code incorrectly

---
+++ script.js
@@ -0,0 +1,66 @@
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
+  // TODO: Add a listener for click events on the save button. This code should
+  // use the id in the containing time-block as a key to save the user input in
+  // local storage. HINT: What does `this` reference in the click listener
+  // function? How can DOM traversal be used to get the "hour-x" id of the
+  // time-block containing the button that was clicked? How might the id be
+  // useful when saving the description in local storage?
+  //
+  // TODO: Add code to apply the past, present, or future class to each time
+  // block by comparing the id to the current hour. HINTS: How can the id
+  // attribute of each time-block be used to conditionally add or remove the
+  // past, present, and future classes? How can Day.js be used to get the
+  // current hour in 24-hour time?
+  //
+  // TODO: Add code to get any user input that was saved in localStorage and set
+  // the values of the corresponding textarea elements. HINT: How can the id
+  // attribute of each time-block be used to do this?
+  //
+  // TODO: Add code to display the current date in the header of the page.
+
+  $(document).ready(function(){
+    // Grab div for timer and get data from dayjs
+    var currentTime = $('#time')
+    var currentDate = dayjs().format("dddd, MMMM D") 
+        currentTime.text(currentDate)
+  
+    // Grab buttons to save and set in local storage
+    var buttonSave = $('.saveBtn')
+    buttonSave.on("click", function(event){
+      event.preventDefault()
+    var timeArea = $(this).attr("id")
+    var calendarText = $(this).siblings(".col-8").val()
+    localStorage.setItem(timeArea, calendarText)
+    taskReturn()
+    })
+  // Grab data from local storage and reinsert to page
+  taskReturn()
+  function taskReturn(){
+    for (var i=9; i<18; i++){
+      var calendarText = localStorage.getItem(i)
+      $("#" + i + "").text(calendarText)
+    }
+  }
+  // Add the color feature, hint - its an else if statement, going to use the currentTime variable to compare. Also need the hour, access the id, refer to line 33 for the access variable. 
+  timeColor()
+  function timeColor(){
+    var blockHour = $(".description")
+    var currentHour = dayjs().hour()
+    blockHour.each(function(){
+      var hour = $(this).attr("id")
+      console.log(hour)
+      console.log(currentTime)
+      if (currentHour > hour) {
+        $(this).addClass("past")
+      }
+      else if (currentHour == hour) {
+        $(this).addClass("present")
+      }
+      else if (currentHour < hour) {
+        $(this).addClass("future")
+      }
+    })
+  }
+  })
